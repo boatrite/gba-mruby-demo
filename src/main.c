fd4bb438d483d7c02685cd8d1f5d9fb0d75fd194
Gwen Boatrite <gwen.boatrite@gmail.com>
2020-10-12 11:33:34 -0500

    Run demo from Ruby
    TAG: 03-run-demo-from-ruby

--- src/main.c
+++ src/main.c
@@ -1,19 +1,9 @@
 #include <mruby.h>
-#include <mruby/class.h>
 #include <mruby/compile.h>
-#include <mruby/irep.h>
 
-mrb_state *new_ruby_vm() {
-  // Start Ruby VM
-  mrb_state *mrb = mrb_open();
-
-  return mrb;
-}
-
-int main(void) {
-  mrb_state *mrb = new_ruby_vm();
-  mrb_close(mrb);
-
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wunused-parameter"
+mrb_value run_demo(mrb_state *mrb, mrb_value self) {
   // Write into the I/O registers, setting video display parameters.
   volatile unsigned char *ioram = (unsigned char *)0x04000000;
   ioram[0] = 0x03; // Use video mode 3 (in BG2, a 16bpp bitmap in VRAM)
@@ -23,7 +13,28 @@ int main(void) {
   volatile unsigned short *vram = (unsigned short *)0x06000000;
   vram[80 * 240 + 115] = 0x001F; // X = 115, Y = 80, C = 000000000011111 = R
   vram[80 * 240 + 120] = 0x03E0; // X = 120, Y = 80, C = 000001111100000 = G
-  vram[80 * 240 + 125] = 0x7C00; // X = 125, Y = 80, C = 111110000000000 = B
+  vram[80 * 240 + 145] = 0x7C00; // X = 125, Y = 80, C = 111110000000000 = B
+
+  return mrb_nil_value();
+}
+#pragma GCC diagnostic pop
+
+mrb_state *new_ruby_vm() {
+  // Start Ruby VM
+  mrb_state *mrb = mrb_open();
+
+  // Define Ruby extensions backed by C
+  struct RClass *GBA_class = mrb_define_class(mrb, "GBA", mrb->object_class);
+
+  mrb_define_class_method(mrb, GBA_class, "run_demo", run_demo, MRB_ARGS_NONE());
+
+  return mrb;
+}
+
+int main(void) {
+  mrb_state *mrb = new_ruby_vm();
+  mrb_load_string(mrb, "GBA.run_demo");
+  mrb_close(mrb);
 
   // Wait forever
   while (1) {
