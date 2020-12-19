89e814acce6ae51a6968b55e926772b41cda8b9c
Gwen Boatrite <gwen.boatrite@gmail.com>
2020-12-19 13:10:25 -0600

    Port demo to ruby
    TAG: 06-port-fancier-demo-to-ruby

--- scripts/ruby_main.rb
+++ scripts/ruby_main.rb
@@ -1,1 +1,147 @@
-# GBA.run_demo
+class GBA
+  module Input
+    A = 1
+    B = 2
+    SELECT = 4
+    START = 8
+    RIGHT = 16
+    LEFT = 32
+    UP = 64
+    DOWN = 128
+    R = 256
+    L = 512
+  end
+
+  def self.set_ioram(index, value)
+    set_char_memory(GBA::MEM_IO, index, value)
+  end
+
+  def self.set_vram(index, value)
+    set_short_memory(GBA::MEM_VRAM, index, value)
+  end
+
+  def self.input_pressed?(input)
+    key_states & input != 0
+  end
+end
+
+class RubyMain
+  def self.call
+    unless GBA::NDEBUG
+      debug
+      return
+    end
+
+    # --- "Pong" ---
+
+    # Write the color palette for our sprites into the first palette of
+    # 16 colors in color palette memory (this palette has index 0)
+    GBA.set_object_palette_memory(1, 0x1F, 0x1F, 0x1F) # White
+    GBA.set_object_palette_memory(2, 0x1F, 0x00, 0x1F) # Magenta
+
+    # Write the tiles for our sprites into the fourth tile block in VRAM.
+    # Four tiles for an 8x32 paddle sprite, and one tile for an 8x8 ball
+    # sprite. Using 4bpp, 0x1111 is four pixels of colour index 1, and
+    # 0x2222 is four pixels of colour index 2.
+    GBA.set_tile_memory(1, 4, 0x1111) # paddle
+    GBA.set_tile_memory(5, 1, 0x2222) # ball
+
+    # 4bpp tiles, TALL shape
+    # 8x32 size when using the TALL shape
+    # Start at the first tile in tile
+    # block four, use color palette zero
+    GBA.set_obj_attrs(0, 0x8000, 0x4000, 1) # 0 = paddle index
+
+    # 4bpp tiles, SQUARE shape
+    # 8x8 size when using the SQUARE shape
+    # Start at the fifth tile in tile block four,
+    # use color palette zero
+    GBA.set_obj_attrs(1, 0, 0, 5) # 1 = ball index
+
+    player_width = 8
+    player_height = 32
+    ball_width = 8
+    ball_height = 8
+    player_velocity = 2
+    ball_velocity_x = 2
+    ball_velocity_y = 1
+    player_x = 5
+    player_y = 96
+    ball_x = 22
+    ball_y = 96
+
+    GBA.set_object_position(0, player_x, player_y) # 0 = paddle index
+    GBA.set_object_position(1, ball_x, ball_y) # 1 = ball index
+
+    # Set the display parameters to enable objects, and use a 1D
+    # object->tile mapping
+    GBA.reg_display = 0x1000 | 0x0040
+
+    loop do
+      while GBA.reg_display_vcount >= 160
+        # Wait till VDraw
+      end
+      while GBA.reg_display_vcount < 160
+        # Wait till VBlank
+      end
+
+      player_max_clamp_y = GBA::SCREEN_HEIGHT - player_height
+
+      if GBA.input_pressed? GBA::Input::UP
+        player_y = (player_y - player_velocity).clamp(0, player_max_clamp_y)
+      end
+
+      if GBA.input_pressed? GBA::Input::DOWN
+        player_y = (player_y + player_velocity).clamp(0, player_max_clamp_y)
+      end
+
+      if GBA.input_pressed?(GBA::Input::UP) || GBA.input_pressed?(GBA::Input::DOWN)
+        GBA.set_object_position(0, player_x, player_y)
+      end
+
+      ball_max_clamp_x = GBA::SCREEN_WIDTH - ball_width
+      ball_max_clamp_y = GBA::SCREEN_HEIGHT - ball_height
+      if (ball_x >= player_x && ball_x <= player_x + player_width) &&
+          (ball_y >= player_y && ball_y <= player_y + player_height)
+        ball_x = player_x + player_width
+        ball_velocity_x = -ball_velocity_x
+      else
+        if ball_x == 0 || ball_x == ball_max_clamp_x
+          ball_velocity_x = -ball_velocity_x
+        end
+        if ball_y == 0 || ball_y == ball_max_clamp_y
+          ball_velocity_y = -ball_velocity_y
+        end
+      end
+
+      ball_x = (ball_x + ball_velocity_x).clamp(0, ball_max_clamp_x)
+      ball_y = (ball_y + ball_velocity_y).clamp(0, ball_max_clamp_y)
+      GBA.set_object_position(1, ball_x, ball_y)
+    end
+  end
+
+  def self.debug
+    GBA.enable_console
+
+    puts "Ruby loaded. Execution starting."
+
+    puts "NDEBUG: #{GBA::NDEBUG}"
+    puts "MEM_IO: #{GBA::MEM_IO.to_s(16)}"
+    puts "MEM_VRAM: #{GBA::MEM_VRAM.to_s(16)}"
+
+    loop do
+      # puts "reg_display_vcount: #{GBA.reg_display_vcount}"
+
+      puts "A Held" if GBA.input_pressed?(GBA::Input::A)
+      puts "B Held" if GBA.input_pressed?(GBA::Input::B)
+      puts "Select Held" if GBA.input_pressed?(GBA::Input::SELECT)
+      puts "Start Held" if GBA.input_pressed?(GBA::Input::START)
+      puts "Right Held" if GBA.input_pressed?(GBA::Input::RIGHT)
+      puts "Left Held" if GBA.input_pressed?(GBA::Input::LEFT)
+      puts "Up Held" if GBA.input_pressed?(GBA::Input::UP)
+      puts "Down Held" if GBA.input_pressed?(GBA::Input::DOWN)
+      puts "R Held" if GBA.input_pressed?(GBA::Input::R)
+      puts "L Held" if GBA.input_pressed?(GBA::Input::L)
+    end
+  end
+end
--- src/main.c
+++ src/main.c
@@ -1,3 +1,13 @@
+#ifndef NDEBUG
+#define NDEBUG true
+#endif
+
+#include <gba_console.h>
+#include <gba_input.h>
+#include <gba_interrupt.h>
+#include <gba_systemcalls.h>
+#include <gba_video.h>
+
 #include <mruby.h>
 #include <mruby/irep.h>
 
@@ -54,21 +64,6 @@ mrb_value run_demo(mrb_state *mrb, mrb_value self) {
 }
 #pragma GCC diagnostic pop
 
-mrb_state *new_ruby_vm() {
-  // Start Ruby VM
-  mrb_state *mrb = mrb_open();
-
-  // Define Ruby extensions backed by C
-  struct RClass *GBA_class = mrb_define_class(mrb, "GBA", mrb->object_class);
-
-  mrb_define_class_method(mrb, GBA_class, "run_demo", run_demo, MRB_ARGS_NONE());
-
-  // Load in our mruby bytecode
-  mrb_load_irep(mrb, ruby_main);
-
-  return mrb;
-}
-
 // Form a 16-bit BGR GBA colour from three component values
 static inline rgb15 RGB15(int r, int g, int b)
 {
@@ -84,115 +79,142 @@ static inline void set_object_position(volatile obj_attrs *object, int x, int y)
     (x & OBJECT_ATTR1_X_MASK);
 }
 
-// Clamp 'value' in the range 'min' to 'max' (inclusive)
-static inline int clamp(int value, int min, int max)
-{
-  return (value < min ? min
-      : (value > max ? max : value));
+mrb_value setTileMemory(mrb_state *mrb, mrb_value self) {
+  int index;
+  int count;
+  int value;
+  mrb_get_args(mrb, "iii", &index, &count, &value);
+
+  /* printf("index: %d\n", index); */
+  /* printf("count: %d\n", count); */
+  /* printf("value: %x\n", value); */
+
+  volatile uint16_t *sprite_tile_mem = (uint16_t *)TILE_MEM[4][index];
+
+  for (int i = 0; i < count * (sizeof(tile_4bpp) / 2); ++i) {
+    sprite_tile_mem[i] = value; // e.g. 0x1111 == 0b_0001_0001_0001_0001
+  }
+
+  return mrb_nil_value();
+}
+
+mrb_value setObjectPaletteMemory(mrb_state *mrb, mrb_value self) {
+  int index;
+  int red;
+  int green;
+  int blue;
+  mrb_get_args(mrb, "iiii", &index, &red, &green, &blue);
+
+  OBJECT_PALETTE_MEM[index] = RGB15(red, green, blue);
+
+  return mrb_nil_value();
+}
+
+mrb_value setObjAttrs(mrb_state *mrb, mrb_value self) {
+  int index;
+  int attr0;
+  int attr1;
+  int attr2;
+  mrb_get_args(mrb, "iiii", &index, &attr0, &attr1, &attr2);
+
+  volatile obj_attrs *attrs = &OAM_MEM[index];
+  attrs->attr0 = attr0;
+  attrs->attr1 = attr1;
+  attrs->attr2 = attr2;
+
+  return mrb_nil_value();
+}
+
+mrb_value setObjectPosition(mrb_state *mrb, mrb_value self) {
+  int index;
+  int x;
+  int y;
+  mrb_get_args(mrb, "iii", &index, &x, &y);
+
+  volatile obj_attrs *attrs = &OAM_MEM[index];
+  set_object_position(attrs, x, y);
+
+  return mrb_nil_value();
+}
+
+mrb_value setRegDisplay(mrb_state *mrb, mrb_value self) {
+  int value;
+  mrb_get_args(mrb, "i", &value);
+
+  REG_DISPLAY = value;
+
+  return mrb_nil_value();
+}
+
+mrb_value setShortMemory(mrb_state *mrb, mrb_value self) {
+  volatile unsigned short *address;
+  int index;
+  int value;
+  mrb_get_args(mrb, "iii", &address, &index, &value);
+  address[index] = value;
+  return mrb_nil_value();
+}
+
+mrb_value setCharMemory(mrb_state *mrb, mrb_value self) {
+  volatile unsigned char *address;
+  int index;
+  int value;
+  mrb_get_args(mrb, "iii", &address, &index, &value);
+  address[index] = value;
+  return mrb_nil_value();
+}
+
+mrb_value enable_console(mrb_state *mrb, mrb_value self) {
+  irqInit();
+  irqEnable(IRQ_VBLANK);
+  // Eventually I bet I can lookup what consoleDemoInit does, and find a way to
+  // reverse it, and eventually flip back and forth between them maybe
+  consoleDemoInit();
+}
+
+mrb_value key_states(mrb_state *mrb, mrb_value self) {
+  return mrb_fixnum_value(~REG_KEY_INPUT & KEY_ANY);
+}
+
+mrb_value reg_display_vcount(mrb_state *mrb, mrb_value self) {
+  return mrb_fixnum_value(REG_DISPLAY_VCOUNT);
+}
+
+mrb_state *new_ruby_vm() {
+  // Start Ruby VM
+  mrb_state *mrb = mrb_open();
+
+  // Define Ruby extensions backed by C
+  struct RClass *GBA_class = mrb_define_class(mrb, "GBA", mrb->object_class);
+
+  mrb_define_class_method(mrb, GBA_class, "run_demo", run_demo, MRB_ARGS_NONE());
+
+  mrb_define_class_method(mrb, GBA_class, "set_short_memory", setShortMemory, MRB_ARGS_REQ(3));
+  mrb_define_class_method(mrb, GBA_class, "set_char_memory", setCharMemory, MRB_ARGS_REQ(3));
+  mrb_define_class_method(mrb, GBA_class, "set_tile_memory", setTileMemory, MRB_ARGS_REQ(3));
+  mrb_define_class_method(mrb, GBA_class, "set_object_palette_memory", setObjectPaletteMemory, MRB_ARGS_REQ(4));
+  mrb_define_class_method(mrb, GBA_class, "set_obj_attrs", setObjAttrs, MRB_ARGS_REQ(4));
+  mrb_define_class_method(mrb, GBA_class, "set_object_position", setObjectPosition, MRB_ARGS_REQ(3));
+  mrb_define_class_method(mrb, GBA_class, "reg_display=", setRegDisplay, MRB_ARGS_REQ(1));
+  mrb_define_class_method(mrb, GBA_class, "enable_console", enable_console, MRB_ARGS_NONE());
+  mrb_define_class_method(mrb, GBA_class, "key_states", key_states, MRB_ARGS_NONE());
+  mrb_define_class_method(mrb, GBA_class, "reg_display_vcount", reg_display_vcount, MRB_ARGS_NONE());
+
+  mrb_define_const(mrb, GBA_class, "NDEBUG", mrb_bool_value(NDEBUG));
+  mrb_define_const(mrb, GBA_class, "SCREEN_HEIGHT", mrb_fixnum_value(SCREEN_HEIGHT));
+  mrb_define_const(mrb, GBA_class, "SCREEN_WIDTH", mrb_fixnum_value(SCREEN_WIDTH));
+  mrb_define_const(mrb, GBA_class, "MEM_IO", mrb_fixnum_value(MEM_IO));
+  mrb_define_const(mrb, GBA_class, "MEM_VRAM", mrb_fixnum_value(MEM_VRAM));
+
+  // Load in our mruby bytecode
+  mrb_load_irep(mrb, ruby_main);
+
+  return mrb;
 }
 
 int main(void) {
   mrb_state *mrb = new_ruby_vm();
+  mrb_load_string(mrb, "RubyMain.call");
   mrb_close(mrb);
-
-  // Write the tiles for our sprites into the fourth tile block in VRAM.
-  // Four tiles for an 8x32 paddle sprite, and one tile for an 8x8 ball
-  // sprite. Using 4bpp, 0x1111 is four pixels of colour index 1, and
-  // 0x2222 is four pixels of colour index 2.
-  //
-  // NOTE: We're using our own memory writing code here to avoid the
-  // byte-granular writes that something like 'memset' might make (GBA
-  // VRAM doesn't support byte-granular writes).
-  volatile uint16_t *paddle_tile_mem = (uint16_t *)TILE_MEM[4][1];
-  volatile uint16_t *ball_tile_mem   = (uint16_t *)TILE_MEM[4][5];
-  for (int i = 0; i < 4 * (sizeof(tile_4bpp) / 2); ++i)
-    paddle_tile_mem[i] = 0x1111; // 0b_0001_0001_0001_0001
-  for (int i = 0; i < (sizeof(tile_4bpp) / 2); ++i)
-    ball_tile_mem[i] = 0x2222;   // 0b_0002_0002_0002_0002
-
-  // Write the colour palette for our sprites into the first palette of
-  // 16 colours in colour palette memory (this palette has index 0)
-  OBJECT_PALETTE_MEM[1] = RGB15(0x1F, 0x1F, 0x1F); // White
-  OBJECT_PALETTE_MEM[2] = RGB15(0x1F, 0x00, 0x1F); // Magenta
-
-  // Create our sprites by writing their object attributes into OAM
-  // memory
-  volatile obj_attrs *paddle_attrs = &OAM_MEM[0];
-  paddle_attrs->attr0 = 0x8000; // 4bpp tiles, TALL shape
-  paddle_attrs->attr1 = 0x4000; // 8x32 size when using the TALL shape
-  paddle_attrs->attr2 = 1;      // Start at the first tile in tile
-  // block four, use color palette zero
-  volatile obj_attrs *ball_attrs = &OAM_MEM[1];
-  ball_attrs->attr0 = 0; // 4bpp tiles, SQUARE shape
-  ball_attrs->attr1 = 0; // 8x8 size when using the SQUARE shape
-  ball_attrs->attr2 = 5; // Start at the fifth tile in tile block four,
-  // use color palette zero
-
-  // Initialize variables to keep track of the state of the paddle and
-  // ball, and set their initial positions (by modifying their
-  // attributes in OAM)
-  const int player_width = 8,
-        player_height = 32;
-  const int ball_width = 8,
-        ball_height = 8;
-  int player_velocity = 2;
-  int ball_velocity_x = 2,
-      ball_velocity_y = 1;
-  int player_x = 5,
-      player_y = 96;
-  int ball_x = 22,
-      ball_y = 96;
-  set_object_position(paddle_attrs, player_x, player_y);
-  set_object_position(ball_attrs, ball_x, ball_y);
-
-  // Set the display parameters to enable objects, and use a 1D
-  // object->tile mapping
-  REG_DISPLAY = 0x1000 | 0x0040;
-
-  // The main game loop
-  uint32_t key_states = 0;
-  while (1) {
-    // Skip past the rest of any current V-Blank, then skip past
-    // the V-Draw
-    while(REG_DISPLAY_VCOUNT >= 160);
-    while(REG_DISPLAY_VCOUNT <  160);
-
-    // Get current key states (REG_KEY_INPUT stores the states
-    // inverted)
-    key_states = ~REG_KEY_INPUT & KEY_ANY;
-
-    // Note that our physics update is tied to the framerate,
-    // which isn't generally speaking a good idea. Also, this is
-    // really terrible physics and collision handling code.
-    int player_max_clamp_y = SCREEN_HEIGHT - player_height;
-    if (key_states & KEY_UP)
-      player_y = clamp(player_y - player_velocity, 0,
-          player_max_clamp_y);
-    if (key_states & KEY_DOWN)
-      player_y = clamp(player_y + player_velocity, 0,
-          player_max_clamp_y);
-    if (key_states & KEY_UP || key_states & KEY_DOWN)
-      set_object_position(paddle_attrs, player_x, player_y);
-
-    int ball_max_clamp_x = SCREEN_WIDTH  - ball_width,
-        ball_max_clamp_y = SCREEN_HEIGHT - ball_height;
-    if ((ball_x >= player_x &&
-          ball_x <= player_x + player_width) &&
-        (ball_y >= player_y &&
-         ball_y <= player_y + player_height)) {
-      ball_x = player_x + player_width;
-      ball_velocity_x = -ball_velocity_x;
-    } else {
-      if (ball_x == 0 || ball_x == ball_max_clamp_x)
-        ball_velocity_x = -ball_velocity_x;
-      if (ball_y == 0 || ball_y == ball_max_clamp_y)
-        ball_velocity_y = -ball_velocity_y;
-    }
-
-    ball_x = clamp(ball_x + ball_velocity_x, 0, ball_max_clamp_x);
-    ball_y = clamp(ball_y + ball_velocity_y, 0, ball_max_clamp_y);
-    set_object_position(ball_attrs, ball_x, ball_y);
-  }
-
   return 0;
 }
